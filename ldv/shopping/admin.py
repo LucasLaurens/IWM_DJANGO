56d8c0d02de56eff91b67be77bec8736268a7a08
LucasLaurens <lucaslaurens.74@gmail.com>
2020-05-25 12:24:07 +0200

    manage display fields in admin

--- ldv/shopping/admin.py
+++ ldv/shopping/admin.py
@@ -1,7 +1,14 @@
 from django.apps import apps
 from django.contrib import admin
 
+from shopping.models import Clothes
 # Register your models here.
+
+@admin.register(Clothes)
+class ClothesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    ordering = ('name', 'price')
+    search_fields = ('name', 'price')
 
 models = apps.get_models()
 
